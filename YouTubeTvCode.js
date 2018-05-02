fb9b68b03a47e349427e7b9e164b3ef6cff1302e
Takashi SASAKI <takashi316@gmail.com>
2018-05-02 15:07:38 +0900

    puppeteer@0.9.0

--- YouTubeTvCode.js
+++ YouTubeTvCode.js
@@ -3,6 +3,24 @@ const puppeteer = require("puppeteer");
 
 const options = [
 {
+  executablePath: "/usr/bin/chromium",
+  headless: false,
+  waitUntil: 'networkidle',
+  networkIdleTimeout: 5000,
+  userDataDir: "/tmp/puppeteer",
+  args: ['--start-fullscreen', "---no-first-run", "--no-default-browse-check",
+  "--no-sandbox"]
+},
+{
+  executablePath: "/usr/bin/chromium-browser",
+  headless: false,
+  waitUntil: 'networkidle',
+  networkIdleTimeout: 5000,
+  userDataDir: "/tmp/puppeteer",
+  args: ['--start-fullscreen', "---no-first-run", "--no-default-browse-check",
+  "--no-sandbox"]
+},
+{
   executablePath: "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe",
   headless: false,
   waitUntil: 'networkidle',
@@ -18,39 +36,53 @@ const options = [
   userDataDir: "C:\\tmp\\puppeteer",
   args: ['--start-fullscreen']
 },
-{
-  executablePath: "chromium-browser",
-  headless: false,
-  waitUntil: 'networkidle',
-  networkIdleTimeout: 5000,
-  userDataDir: "/tmp/puppeteer",
-  args: ['--start-fullscreen']
-}
 ];
 
 async function main(){
   for(var i in options) {
     try{
+      console.log(options[i].executablePath);
       var browser = await puppeteer.launch(options[i]);
+      console.log("main¥t: succeeded to execute chromium/chrome.");
+      console.log("main^t: typeof browser = " + typeof browser);
       break;
     } catch(e) {
+      console.log("main¥t: failed to execute chromium/chrome");
       continue;
     }//try
   }//for
 
+  if(typeof browser === "undefined") {
+    console.log("main¥t: all options failed. exiting... ");
+    process.exit(-1);
+  }//if
+
+  console.log("main¥t: await browser.newPage()");
   const page = await browser.newPage();
+  console.log("main^t: waiting for new page.");
+  await page.waitFor(5000);
+  console.log("main¥t: await page.goto(...)");
   await page.goto("https://www.youtube.com/tv#/settings?resume");
-  await page.waitFor(1000);
-  const x = await page.$("#settings-items > div.content > div.focused.new-list.settings-list > div > div:nth-child(2)");
-  x.click();
-  await page.waitFor(1000);
+  await page.waitFor(5000);
+  console.log("main¥t: await page.$(...)");
+  const x = await page.$("#settings-items > div.content > div.settings-list > div > div:nth-child(2)");
+  if(x === null) {
+    console.log("main¥t: failed to get the element.");
+    process.exit();
+  }//if
+  console.log("main¥t: await x.click()");
+  await x.click();
+  await page.waitFor(2000);
+  console.log("main¥t: await page.$(...)");
   const y = await page.$("#dialog-view > div > div.left-column.divider > div > div > div > div.button:nth-child(2) > span");
-  y.click();
+  console.log("main¥t: await y.clock()");
+  await y.click();
   await page.waitFor(1000);
+  console.log("main¥t: finished");
 }//main
 
 main().then(x => {
   console.log(x);
-  process.exit();
+  //process.exit();
 }).catch(e=>console.log(e));
 
