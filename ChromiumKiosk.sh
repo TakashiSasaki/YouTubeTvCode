7e1fbb151645c6dc94ed4fe3f34dc0356e907ff7
Takashi SASAKI <takashi316@gmail.com>
2018-05-09 17:28:46 +0900

    fix chromium profile directory

--- ChromiumKiosk.sh
+++ ChromiumKiosk.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
-sed -i -e 's/"exited_cleanly":false/"exited_clanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i -e 's/"exit_type":"Crashed"/"ext_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-sed -i -e 's/"exited_cleanly":false/"exited_clanly":true/' /tmp/puppeteer/Default/Preferences
-sed -i -e 's/"exit_type":"Crashed"/"ext_type":"Normal"/' /tmp/puppeteer/Default/Preferences
 
-chromium-browser  --start-fullscreen --disable-infobars --disable-session-crashed-bubble --disable-session-restore --noerrdialogs --no-first-run --user-data-dir=/tmp/puppeteer https://youtube.com/tv
+PROFILE=$HOME/.config/chromium/Default
+PREFERENCES=$PROFILE/Preferences
+
+ps aux | grep chrom | wc
+if [ ! -e $PREFERENCES ]; then exit -1; fi
+sync
+sed -i -e 's/"exited_cleanly":false/"exited_clanly":true/' $PREFERENCES
+sed -i -e 's/"exit_type":"Crashed"/"ext_type":"Normal"/' $PREFERENCES
+sync
+
+CHROMIUMOPTIONS=" --start-fullscreen --disable-infobars --disable-session-crashed-bubble  --disable-session-restore  --noerrdialogs  --no-first-run"
+
+sleep 1
+chromium-browser ${CHROMIUMOPTIONS} --user-data-dir=$PROFILE https://youtube.com/tv
 
