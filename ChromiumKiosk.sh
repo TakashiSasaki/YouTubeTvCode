1998cd2bdda11e8362f23a0425e7cbe144e073d8
Takashi SASAKI <takashi316@gmail.com>
2018-05-11 06:19:26 +0900

    fix --user-data-dir and disable DPMS

--- ChromiumKiosk.sh
+++ ChromiumKiosk.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-PROFILE=$HOME/.config/chromium/Default
+CONFIG=$HOME/.config/chromium
+PROFILE=$CONFIG/Default
 PREFERENCES=$PROFILE/Preferences
 
 ps aux | grep chrom | wc
-if [ ! -e $PREFERENCES ]; then exit -1; fi
 sync
 sed -i -e 's/"exited_cleanly":false/"exited_clanly":true/' $PREFERENCES
 sed -i -e 's/"exit_type":"Crashed"/"ext_type":"Normal"/' $PREFERENCES
@@ -14,5 +14,11 @@ sync
 CHROMIUMOPTIONS=" --start-fullscreen --disable-infobars --disable-session-crashed-bubble  --disable-session-restore  --noerrdialogs  --no-first-run"
 
 sleep 1
-chromium-browser ${CHROMIUMOPTIONS} --user-data-dir=$PROFILE https://youtube.com/tv
+chromium-browser ${CHROMIUMOPTIONS} --user-data-dir=${CONFIG} https://youtube.com/tv
+
+# disabloe screensaver
+xset s s off
+# dpsable Display Power Management System (Energy Star)
+
+xset -dpms
 
